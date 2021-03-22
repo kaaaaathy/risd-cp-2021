8851d122684f91d62062d520414ec76227f65c43
kathy wu <kathy@scratch.mit.edu>
2021-03-21 23:45:14 -0400

    Revert "🔮🌷 Updated with Glitch (test)"

---
+++ data.js
@@ -0,0 +1,108 @@
+export default
+[ {
+  shown:false,
+  date:"Due 3/01",
+  project:"Project 0",
+  name:"Re-Indexing",
+  content:`
+  As a class, we are going to create an slack index of rewritten computer history materials.
+  <br><br>
+  For this 1-week assignment, find 1-3 articles/links/zines/media that you would like in a syllabus about computer history. How would we incubate a less white supremacist, patriarchal computer history?
+  <br><br>
+  <img src="https://kaaathy.com/risd-cp-2021/imgs/abtec.jpg" class="imageCaption"><br><span class="imageCaption">ABTEC Island, Skawennati</span>
+  <br><br>
+  Post these to the working google doc: <a href="https://tinyurl.com/comppoetics-reindex" target="_blank">https://tinyurl.com/comppoetics-reindex</a>. If someone posts yours, feel free to repost to emphasize; try to find another. We will discuss/respond to each link in class 2.
+  `
+  },
+
+  {
+    shown:false,
+    date:"Due 3/08",
+    project:"Project 1",
+    name:"Transcription",
+    content:`
+    Transcribe 500-750 words of any text. It must be done by you, manually. This project is less about form and more about experience + process.
+    <br>
+    <br>
+    Consider your source text:
+    <li>Is it already presented as digital, parsable text?
+    <li>Is it archived as a printed or written object (or otherwise not-presently machine readable)?
+    <li>How spatial is it? (e.g. a set of unrelated receipts on the table, a set of street names)
+    <br>
+    <br>
+    <img src="https://kaaathy.com/risd-cp-2021/imgs/Bruno-Leyval.jpg" class="imageCaption"><br><span class="imageCaption">Automatic Writing, Bruno Leyval</span>
+    <br><br>
+    In a few sentences in the Slack, articulate how transcription feels as a method of transformation. Note the bodily and mental experience of translation. Note the decisions you make when you are translating it. In which ways does it feel neutral, or opinionated?
+`
+  },
+{
+		shown:false,
+    date:"Due 3/29",
+		project:"Project 2",
+    name:"Programmatic / Poetic",
+		content:`
+    Create a text-based project with a web-based component which applies computational ingredients (i.e. chance, score, ordering) to text and optionally, imagery. This project:
+    <br>
+    <br>
+    <li> Must be able to be read in class by another person, without instruction. (If it’s important for it to be read by you, let me know)
+    <li> Must, at minimum, use randomness in some way
+    <li> Must demonstrate a basic knowledge of HTML/CSS and calling an array by index
+    <li> You can use an existing text (i.e. a poem, piece of news, etc) or you can write your own.
+    <br>
+    <br>
+    <img src="https://kaaathy.com/risd-cp-2021/imgs/ants.jpg" class="imageCaption"><br><span class="imageCaption">Benjamin Patterson, Ants, 1960-1962</span>
+    <br><br>
+    <b>To consider:</b> How do computational ingredients affect the nature of the text? How does the literal and metaphorical concepts randomness, repetition, score transform the text’s meaning?
+    `
+	},
+	{
+		shown:false,
+    date:"Due 4/19",
+		project:"Project 3",
+    name:"Embodied Voice(s)",
+		content:`
+    Create a project with a focus on embodiment/interface/avatar and collective voice. This project:
+    <br>
+    <br>
+    <li>Must draw a relationship between two or more pieces of text by different authors (one of them can be you)
+    <li>Must be performed for the class over zoom (or you can share a recording of the performance)
+    <li>Can use any medium of language expression: text, audio based, etc. Will you perform it? Will you use a voice interface other than yourself? Will it be silent?
+    <br>
+    <br>
+    <img src="https://kaaathy.com/risd-cp-2021/imgs/siri.jpg" class="imageCaption"><br><span class="imageCaption">Siri</span>
+    <br><br>
+    <b>To consider:</b> How does the embodiment of the text shift the relationship between the original text(s)? Will you reveal difference between all/both voices, or channel through a disambiguating singular body?
+    `
+
+	},
+
+	{
+		shown:false,
+    date:"Due 5/10",
+		project:"Project 4",
+    name:"New Methods",
+		content:`
+    Create a new score, procedure, tool, and/or object for reading/writing. "Reading" and "writing" here can be interpreted broadly, i.e. "reading" as data collection, etc.
+    <br>
+    <br>
+    <b>Articulate:</b><br>
+    <li>Your corpus
+    <li>Your methods
+    <li>Mode of embodiment/performance/interface
+    <br><br>
+    Present the methods as a documented source with the final piece. This can be as simple as a .txt doc alongside it, or it can be embedded in the performance.
+    <br><br>
+    <b>Consider:</b><br>
+    <li>Is the result different each time on the same text(s), or the same?
+    <li>How does it behave on different texts?
+    <li>Can / should this be performed through other means?
+    <br><br>
+    <img src="https://kaaathy.com/risd-cp-2021/imgs/ouroboros.jpg" class="imageCaption"><br><span class="imageCaption">Image of an ouroboros, or serpent eating its tail. (Wikipedia)</span>
+    <br><br>
+    This work can build off of one of your previous projects. Themes from class you can optionally play with if it inspires: Missing data sets. obfuscation of secrets, labor / work
+
+
+    `
+	},
+
+]
